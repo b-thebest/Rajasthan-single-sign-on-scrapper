ff42e1e1b8de8cc96fc9741afbe64af91dbd910e
bkamlapurwala <bkamlapurwala@zolute.work>
2019-11-05 18:59:33 +0530

    Exception handeled for more than one login from on ip form is getting filled but having timer issue with each field

--- rjbot.py
+++ rjbot.py
@@ -9,6 +9,8 @@ from selenium.common.exceptions import TimeoutException
 import pandas as pd
 import time
 
+s1 = pd.read_excel('person_details.xlsx', 'Sheet1')
+
 #Defining browser
 #browser = webdriver.Chrome(str(os.getcwd()) + "/chromedriver")
 browser = webdriver.Firefox()
@@ -32,35 +34,109 @@ except:
     print('no pop up was found')
 
 # Second form ------
-while True:
-    try:
-        browser.find_element_by_xpath('//*[@id="cpBody_dlApplications_LinkButton2_0"]').click()
-        break
-    except:
-        time.sleep(1)
-while True:
-    try:
-        ticket = browser.find_element_by_class_name('fa.fa-ticket.fa-fw').click()
-        break
-    except:
-        time.sleep(2)
-while True:
-    try:
-        browser.find_element_by_xpath('//a[@href="'+'/BookOnlineTicket/BookOnlineTicket?CT=QWR2YW5jZQ=='+'"]').click()
-        break
-    except:
-        time.sleep(2)
-time.sleep(2)
-buttons = browser.find_elements_by_class_name("close")
-buttons[1].click()
+#element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "cpBody_dlApplications_LinkButton2_0")))
+try:
+    browser.find_element_by_xpath('//*[@id="cpBody_txt_Data2"]').send_keys('DTT4SHALU')
+    time.sleep(1)
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, 'cpBody_cbx_newsession'))).click()
+    browser.find_element_by_xpath('.//*[@id="cpBody_btn_LDAPLogin"]').click()
+    element = WebDriverWait(browser, 5).until_not(
+        EC.visibility_of_element_located((By.ID, "cpBody_dlApplications_LinkButton2_0")))
+    element.click()
+except:
+    element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, "cpBody_dlApplications_LinkButton2_0")))
+    element.click()
 
+while True:
+    try:
+        '''WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "fa.fa-ticket.fa-fw"))).click()
+        WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.XPATH, "//a[@href='/BookOnlineTicket/BookOnlineTicket?CT=QWR2YW5jZQ==']"))).click()'''
+        elem = browser.find_elements_by_class_name("fa.fa-ticket.fa-fw")
+        elem[0].click()
+        browser.implicitly_wait(1)
+        #WebDriverWait(browser, 10).until(EC.presence_of_element_located(
+         #   (By.XPATH, "//a[@href='/BookOnlineTicket/BookOnlineTicket?CT=QWR2YW5jZQ==']"))).click()
+        browser.find_element_by_xpath('//a[@href="/BookOnlineTicket/BookOnlineTicket?CT=QWR2YW5jZQ=="]').click()
+        break
+    except:
+        time.sleep(2)
+
+try:
+    browser.implicitly_wait(4)
+    buttons = browser.find_elements_by_class_name("close")
+    buttons[1].click()
+except:
+    pass
+
+number_of_seats = None
+while True:
+    text = browser.find_element_by_xpath('//*[@id="lbl_NumberofMembersavailable"]').text
+    if text != '':
+        print(text)
+        number_of_seats = int(text.split(': ')[1])
+        print(number_of_seats)
+        break
+    time.sleep(2)
+
+if number_of_seats > s1['Name'].count():
+    table_id = browser.find_element_by_id('tblMemberInfo')
+    rows = table_id.find_elements(By.XPATH, ".//*[contains(@id,'tbdMemberInfo')]")  # get all of the rows in the table
+    k = 0
+    print("number of rows found ", len(rows))
+    numberOfMembers = s1['Name'].count()
+    print("number of records to enter ", numberOfMembers)
+    maxK = numberOfMembers + 1
+    for j in range(numberOfMembers):
+
+        # name
+        label = 'Member' + 'Name' + str(j)
+        element = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, label)))
+        element.clear()
+        element.send_keys(s1['Name'][j].title())
+        element.send_keys(Keys.TAB)
+
+        # gender
+        label = 'Member' + 'Gender' + str(j)
+        element = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, label)))
+        element.send_keys(s1['Gender'][j].title())
+        element.send_keys(Keys.TAB)
+
+        # nationality
+        label = 'Member' + 'Nationality' + str(j)
+        element = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, label)))
+        element.send_keys(s1['Nationality'][j].title())
+        element.send_keys(Keys.TAB)
+
+        # ID type
+        label = 'Member' + 'IdType' + str(j)
+        element = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, label)))
+        element.send_keys(s1['ID_type'][j])
+        element.send_keys(Keys.TAB)
+
+        # ID No,
+        label = 'Member' + 'IdNo' + str(j)
+        element = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, label)))
+        element.clear()
+        element.send_keys(str(s1['ID_no'][j]))
+        element.send_keys(Keys.TAB)
+
+        # Video camera
+        label = 'Member' + 'TotalCamera' + str(j)
+        element = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, label)))
+        element.clear()
+        element.send_keys(str(s1['Num_of_Camera'][j]))
+        element.send_keys(Keys.TAB)
+
+while True:
+    pass
 
 #check ticket availability
 #fields = browser.find_elements(By.XPATH, ".//*[contains(@class,'col-lg-6')]")
-label = 'ddl_place'
-place = browser.find_element_by_xpath('//select[@id="ddl_place"]')
-place.click()
+'''select = Select(browser.find_element_by_xpath('//*[@id="ddl_place"]'))
+select.select_by_index(1)
 
+browser.find_element_by_xpath("//select[@name='ddl_place']/option[text()='']").click()
 #"//select[@name='element_name']/option[text()='option_text']"
 #element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, label)))
 #select = WebDriverWait(browser,10).until(EC.presence_of_element_located((By.ID, '')))
@@ -94,7 +170,7 @@ element.select_by_visible_text('Gypsy')
 ##BUTTON CLOSE KARNE WALA CLICK KARNA HAI DIALOG BOX HATANE K LIYE
 close = browser.find_elements_by_class_name('close')
 print(len(close))
-#close[4].click()
+#close[4].click()'''
 
 '''
 # Check in
